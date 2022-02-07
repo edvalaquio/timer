346f51c4a125aacddb6874459aba766ca558ae55
Emmanuel Valaquio <v-emmanuel.valaquio@lionbridge.com>
2022-02-08 01:25:39 +0900

    Adding timer form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement, useState } from "react";
 
-function App() {
+type TimerDetails = Readonly<{
+  minutes: number;
+  seconds: number;
+}>;
+
+function TimerForm({
+  index,
+  minutes,
+  seconds,
+}: Readonly<{
+  index: number;
+  minutes: number;
+  seconds: number;
+}>): ReactElement {
   return (
-    <div className="App">
+    <>
+      <h1>{index + 1}</h1>
+      <form>
+        <label>
+          name: <input type="text" name="name" />
+        </label>
+        <label>
+          minute: <input type="text" name="minute" defaultValue={minutes} />
+        </label>
+        <label>
+          second: <input type="text" name="second" defaultValue={seconds} />
+        </label>
+      </form>
+    </>
+  );
+}
+
+function App(): ReactElement {
+  const [timers, setTimer] = useState<ReadonlyArray<TimerDetails>>([]);
+
+  const onAddTimer = () => {
+    const updatedTimers = [
+      ...timers,
+      {
+        minutes: 0,
+        seconds: 0,
+      },
+    ];
+    setTimer(updatedTimers);
+  };
+
+  return (
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Timer</h1>
       </header>
+      <section>
+        <button type="button" onClick={onAddTimer} name="submit">
+          add
+        </button>
+        {timers.map((timerDetails, index) => {
+          return (
+            <React.Fragment key={index}>
+              <TimerForm {...timerDetails} index={index} />
+            </React.Fragment>
+          );
+        })}
+      </section>
     </div>
   );
 }
